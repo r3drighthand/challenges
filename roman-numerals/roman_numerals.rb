a86f3b3703788a483ef70a5282f7161d15b8fd6b
r3drighthand <ryanmccool13@gmail.com>
2018-09-01 11:23:26 -0500

    Add code to roman numerals

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,3 +1,42 @@
-def convert_to_roman(arabic_number)
-  # Your code goes here
+def convert_to_roman(arabic_number, modern = false)
+  old_roman = {
+    "M" => 1000,
+    "D" => 500,
+    "C" => 100,
+    "L" => 50,
+    "X" => 10,
+    "V" => 5,
+    "I" => 1
+  }
+
+  modern_roman = {
+    "M" => 1000,
+    "CM" => 900,
+    "D" => 500,
+    "CD" => 400,
+    "C" => 100,
+    "XC" => 90,
+    "L" => 50,
+    "XL" => 40,
+    "X" => 10,
+    "IX" => 9,
+    "V" => 5,
+    "IV" => 4,
+    "I" => 1
+  }
+
+  if modern
+    hash = modern_roman
+  else
+    hash = old_roman
+  end
+
+  n = arabic_number
+  roman_string = ''
+  hash.each do |key, value|
+    roman_string << key * (n / value)
+    n = n % value
+  end
+  roman_string
 end
+
