717966050e2e50a70ff06b869e227c4725475d58
r3drighthand <ryanmccool13@gmail.com>
2018-09-02 11:34:08 -0500

    Add code to longest string, pad, and rectangle

--- javascript-drills/js/longest_string.js
+++ javascript-drills/js/longest_string.js
@@ -1,3 +1,12 @@
 function longestString(strings) {
-  // Your code here!
+  if(strings.length === 0){
+    return null;
+  }
+  var longString = "";
+  for(var i = 0; i < strings.length; i++){
+    if(longString.length < strings[i].length){
+      longString = strings[i];
+    }
+  }
+  return longString;
 }
