bdb12ba49b630af6d3de0b58c0a231f9a8cf141d
r3drighthand <ryanmccool13@gmail.com>
2018-09-02 12:22:44 -0500

    Ajaxify index, add jquery code to application

--- lucky-ajax/public/js/application.js
+++ lucky-ajax/public/js/application.js
@@ -1,9 +1,23 @@
 $(document).ready(function () {
+  $("form").on("submit", function(e){
+    e.preventDefault();
+    //console.log($(this));
 
-  // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery
-  //   2- prevent the default action for that event from happening
-  //   3- use jQuery to submit an AJAX post to the form's action
-  //   4- when the AJAX post is done, display the new die roll using jQuery
+    var $form = $(this);
+    var method = $form.attr("method");
+    var url = $form.attr("action");
+    var data = $form.serialize();
+
+    $.ajax({
+      method: method;
+      url: url,
+      data: data
+    })
+    .done(function(response){
+      // console.log(response);
+      $("#die-container").html(response);
+    })
+
+  })
 
 });
