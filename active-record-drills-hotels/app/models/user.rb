7812ad5b0a7e40d384cb55254a027d43c91ae616
r3drighthand <ryanmccool13@gmail.com>
2018-09-03 12:04:11 -0500

    Tests passing

--- active-record-drills-hotels/app/models/user.rb
+++ active-record-drills-hotels/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :bookings, foreign_key: :guest_id
+  has_many :booked_rooms, through: :bookings, source: :room
+  has_many :booked_hotels, through: :booked_rooms, source: :hotel
 end
