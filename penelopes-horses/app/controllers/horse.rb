9e13f93602ac5f7653013de281d8385b9c8e8bc1
r3drighthand <ryanmccool13@gmail.com>
2018-09-05 11:45:14 -0500

    Add js script to layout, refactor controller for xhr requests, add partials for show, new, and index

--- penelopes-horses/app/controllers/horse.rb
+++ penelopes-horses/app/controllers/horse.rb
@@ -5,19 +5,36 @@ end
 
 get '/horses/new' do
   @horse = Horse.new
-  erb :"/horses/new"
+  if request.xhr?
+    erb :"horses/_new_horse", layout: false, locals: {horse: @horse}
+  else
+    erb :"horses/new"
+  end
 end
 
 post '/horses' do
   @horse = Horse.new(params[:horse])
-  if @horse.save
-    redirect "/horses/#{@horse.id}"
+  if request.xhr?
+    if @horse.save
+      erb :"horses/_horses", layout: false, locals: {horse: @horse}
+    else
+      status 422
+      body "Unprocessable Entity"
+    end
   else
-    erb :"/horses/new"
+    if @horse.save
+      redirect "/horses/#{@horse.id}"
+    else
+      erb :"horses/new"
+    end
   end
 end
 
 get '/horses/:id' do
   @horse = Horse.find(params[:id])
-  erb :"/horses/show"
+  if request.xhr?
+    erb :'/horses/_show_horse', layout: false, locals: {horse: @horse}
+  else
+    erb :"horses/show"
+  end
 end
