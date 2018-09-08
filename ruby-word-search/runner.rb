d9696bd439797e4edd7733ccf690c65bac480c4d
r3drighthand <ryanmccool13@gmail.com>
2018-09-08 11:24:12 -0500

    Modify runner, add spec code

--- ruby-word-search/runner.rb
+++ ruby-word-search/runner.rb
@@ -1,1 +1,30 @@
 require_relative 'word_search'
+
+require_relative 'word_search'
+
+puzzle = [
+  ["a", "w", "o", "l", "v", "e", "s"],
+  ["s", "o", "a", "w", "a", "h", "p"],
+  ["i", "t", "c", "k", "e", "t", "n"],
+  ["o", "t", "s", "d", "h", "o", "h"],
+  ["s", "e", "h", "g", "s", "t", "a"],
+  ["u", "r", "p", "i", "w", "e", "u"],
+  ["z", "s", "b", "n", "u", "i", "r"]
+]
+ p "when wolves is on a horizontal row"
+ p straight_line_include?("wolves", puzzle)
+ p "when happy is not found in the puzzle"
+ p straight_line_include?("happy", puzzle)
+ p "reverse the word"
+ p straight_line_include?("phaw", puzzle)
+
+ p straight_line_include?("otters", puzzle)
+ p straight_line_include?("asios", puzzle)
+ p straight_line_include?("bison", puzzle)
+
+ p "only edge for diagonal"
+ p location = letter_on_edge("bison", puzzle)
+ word = []
+ p find_diagonal(word, puzzle, location["start_row"], location["start_index"])
+
+p straight_line_include?("bison", puzzle)
--- ruby-word-search/spec/word_search_spec.rb
+++ ruby-word-search/spec/word_search_spec.rb
@@ -1,7 +1,31 @@
 require_relative '../word_search'
 
 describe 'straight_line_include?' do
+  let(:puzzle) { [
+  ["a", "w", "o", "l", "v", "e", "s"],
+  ["s", "o", "a", "w", "a", "h", "p"],
+  ["i", "t", "c", "k", "e", "t", "n"],
+  ["o", "t", "s", "d", "h", "o", "h"],
+  ["s", "e", "h", "g", "s", "t", "a"],
+  ["u", "r", "p", "i", "w", "e", "u"],
+  ["z", "s", "b", "n", "u", "i", "r"]
+] }
+
+  it "check to see if horizontal forward" do
+    expect(straight_line_include?("wolves",puzzle)).to be true
+  end
+  it "check to see if horizontal forward" do
+    expect(straight_line_include?("apple",puzzle)).to be false
+  end
+
+  it "check to see if horizontal backward" do
+    expect(straight_line_include?("phaw",puzzle)).to be true
+  end
+  it "check to see if horizontal backward" do
+    expect(straight_line_include?("wash",puzzle)).to be false
+  end
 end
 
 describe 'snaking_include?' do
 end
+
