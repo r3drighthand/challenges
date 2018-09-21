82078380fb7a10344ac2c3d81df440df329d0219
r3drighthand <ryanmccool13@gmail.com>
2018-09-21 10:56:48 -0500

    Add spec, add solution

--- algorithms-prime-factors/spec/prime_factors_spec.rb
+++ algorithms-prime-factors/spec/prime_factors_spec.rb
@@ -1,4 +1,27 @@
 require_relative '../prime_factors'
 
 describe 'prime_factors' do
+  it 'returns empty if the number is prime' do
+    expect(prime_factors(7)).to eq [7]
+  end
+
+  it 'finds prime factors of 6' do
+    expect(prime_factors(6)).to eq [2, 3]
+  end
+
+  it 'finds prime factors of 8' do
+    expect(prime_factors(8)).to eq [2, 2, 2]
+  end
+
+  it 'finds prime factors of 12' do
+    expect(prime_factors(12)).to eq [2, 2, 3]
+  end
+
+  it 'finds prime factors of 38' do
+    expect(prime_factors(38)).to eq [2, 19]
+  end
+
+  it 'finds prime factors of 123123123' do
+    expect(prime_factors(123123123)).to eq [3, 3, 41, 333667]
+  end
 end
