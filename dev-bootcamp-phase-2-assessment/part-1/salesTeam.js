78f038ee3096db033003471135eb1bab8eec6951
r3drighthand <ryanmccool13@gmail.com>
2018-09-26 16:41:53 -0500

    Add js code to employee and salesTeam files

--- dev-bootcamp-phase-2-assessment/part-1/salesTeam.js
+++ dev-bootcamp-phase-2-assessment/part-1/salesTeam.js
@@ -1,4 +1,21 @@
-/**************************************************************/
-/*   It is required to use Array.prototype methods            */
-/*   when iterating over a sales team's array of employees.   */
-/**************************************************************/
+var SalesTeam = function(employees){
+  this.employees = employees;
+}
+
+SalesTeam.prototype.find = function(name){
+  for(var i = 0; i < this.employees.length; i++){
+    if (this.employees[i].firstName === name){
+      return this.employees[i];
+    }
+  }
+}
+
+SalesTeam.prototype.bonusEarners = function(){
+  var bonusGetters = [];
+  for(var i = 0; i < this.employees.length; i++){
+    if (this.employees[i].totalSales() >= 2500){
+      bonusGetters.push(this.employees[i]);
+    }
+  }
+  return bonusGetters;
+}
