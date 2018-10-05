71e1e0a2c66b9b1c8a101eb8c1223a3181ccaf5a
r3drighthand <ryanmccool13@gmail.com>
2018-10-04 23:38:35 -0500

    Setup done

--- ruby-drills-exploring-scope/dog.rb
+++ ruby-drills-exploring-scope/dog.rb
@@ -1,1 +1,19 @@
-dog.rb
+class Dog
+  def initialize(name, breed, license)
+    @name = name
+    @breed = breed
+    @license = license
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+
+  def license
+    @license
+  end
+end
