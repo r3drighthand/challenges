23a50a13ff5cb75391b6e12a1ea74bf560daf010
r3drighthand <ryanmccool13@gmail.com>
2018-10-09 12:07:35 -0500

    Refactor Company & Employee models, and add Readme-Assets folder.

--- ruby-drills-accessor-methods/employee.rb
+++ ruby-drills-accessor-methods/employee.rb
@@ -1,28 +1,10 @@
 class Employee
+  attr_reader :id
+  attr_accessor :name, :position
 
   def initialize(name, position, id)
     @name     = name
     @position = position
     @id       = id
-  end
-
-  def name
-    @name
-  end
-
-  def name=(new_name)
-    @name = new_name
-  end
-
-  def position
-    @position
-  end
-
-  def position=(new_position)
-    @position = new_position
-  end
-
-  def id
-    @id
   end
 end
